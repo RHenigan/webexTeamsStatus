0059a854b47cbe0cd8cdc940e7404f548e8640e6
Thompson <jthom826@MGC12YV7TYLVDQ.fbpld77.ford.com>
2019-11-08 09:55:00 -0500

    JT Happy Birthday Boberto

--- src/main/java/com/hackerlads/webexTeamsStatus/services/MeetingService.java
+++ src/main/java/com/hackerlads/webexTeamsStatus/services/MeetingService.java
@@ -1,4 +1,19 @@
 package com.hackerlads.webexTeamsStatus.services;
 
+import com.hackerlads.webexTeamsStatus.model.DynamicSchedule;
+
+import java.util.LinkedList;
+
 public class MeetingService {
+
+    private DynamicSchedule dynamicSchedule;
+
+    public MeetingService() {
+        DynamicSchedule dynamicSchedule = new DynamicSchedule();
+    }
+
+    public LinkedList getMeetingAgenda() {
+        return dynamicSchedule.generateExpectedSchedule();
+    }
+
 }
