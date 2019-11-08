147e71271422b5d7c7f3f554a6d239705bd9b443
RHeniz <heniganr1@gmail.com>
2019-11-07 20:13:53 -0500

    Add Test Route for webhook

--- src/main/java/com/hackerlads/webexTeamsStatus/controllers/webhookController.java
+++ src/main/java/com/hackerlads/webexTeamsStatus/controllers/webhookController.java
@@ -1,15 +1,19 @@
 package com.hackerlads.webexTeamsStatus.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping("webhooks")
 public class webhookController {
 
-    @GetMapping()
-    public String test() {
-        return "Hello World";
+    @PostMapping("/messageCreated")
+    public void test(@RequestBody Object msgCreatedJSONBody) {
+        System.out.println("HOOK MESSAGE: " + msgCreatedJSONBody);
+        LinkedHashMap mappedMsg = (LinkedHashMap) msgCreatedJSONBody;
+        LinkedHashMap mappedMsg1  = (LinkedHashMap) mappedMsg.get("data");
+        String msgId = (String) mappedMsg1.get("id");
+        System.out.println("MESSAGE ID: " + msgId);
     }
 }
