2c9a1be292aec433401cdfbcf2a4c0a444b46b82
RHeniz <heniganr1@gmail.com>
2019-11-07 21:26:35 -0500

    Abstract command parseing

--- src/main/java/com/hackerlads/webexTeamsStatus/services/MessageService.java
+++ src/main/java/com/hackerlads/webexTeamsStatus/services/MessageService.java
@@ -12,12 +12,29 @@ public class MessageService {
 
     public void messageHandler(String msgId) {
         String message = (String) webexClient.getMessageDetails(msgId);
+        System.out.println("Message Details: " + message);
         message = message.replace("<div>", "").replace("</div>", "");
+        System.out.println("Message divs removed: " + message);
         if (message.charAt(0) == ':') {
-            String command = message.substring(0, message.indexOf(' '));
+            String command = parseOutCommand(message);
+            System.out.println("Command: " + command);
             if (command.equalsIgnoreCase("help")) {
                 webexClient.createMessage("You have reached the help menu!");
+            } else {
+                System.out.println("Not a Valid Command");
             }
+        } else {
+            System.out.println("Not a Command Message");
         }
+    }
+
+    private String parseOutCommand (String message) {
+        String command = "";
+        if (message.contains(" ")) {
+            command = message.substring(0, message.indexOf(" "));
+        } else {
+            command = message;
+        }
+        return command;
     }
 }
