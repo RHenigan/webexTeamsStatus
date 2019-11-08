b8f6f633388ad6f6fbacef5e2b26aa904bda4252
RHeniz <heniganr1@gmail.com>
2019-11-07 19:24:04 -0500

    Initial Commit

---
+++ src/main/java/com/hackerlads/webexTeamsStatus/WebexTeamsStatusApplication.java
@@ -0,0 +1,13 @@
+package com.hackerlads.webexTeamsStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebexTeamsStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebexTeamsStatusApplication.class, args);
+	}
+
+}
